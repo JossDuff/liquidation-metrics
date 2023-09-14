d3d9c00e3269173e48687015ee2b689fdc32c6a3
jossduff <jduff360@gmail.com>
2023-09-14 12:35:54 -0400

    updated types for tracking individual tokens won/lost

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -8,7 +8,14 @@ import {
 } from "../generated/src/Handlers.gen";
 
 
-import { liquidatorEntity } from "../generated/src/Types.gen";
+import {
+  liquidatorEntity,
+  liquidateeEntity,
+  tokenWonEntity,
+  tokenLostEntity,
+  tokenWonLoaderConfig,
+  tokenLostLoaderConfig
+} from "../generated/src/Types.gen";
 import { CDAI } from "./src/Converters.bs";
 
 // MyAwesomeContractContract_AwesomeEvent_loader(({ event, context }) => {
@@ -16,24 +23,69 @@ import { CDAI } from "./src/Converters.bs";
 // });
 
 CDAIContract_LiquidateBorrow_loader(({ event, context }) => {
-  // load the required liquidator entity
-  context.liquidator.load(event.params.liquidator.toString());
+  let liquidatorAddress: string = event.params.liquidator.toString();
+  let liquidateeAddress: string = event.params.borrower.toString();
+  let ctokenAddress: string = event.params.cTokenCollateral.toString();
+  let tokenWonID: string = liquidatorAddress.concat(ctokenAddress);
+  let tokenLostID: string = liquidateeAddress.concat(ctokenAddress);
+
+  context.liquidator.load(liquidatorAddress);
+  context.liquidatee.load(liquidateeAddress);
+  context.tokenWon.load(tokenWonID, { loaders: { loadLiquidator: true } });
+  context.tokenLost.load(tokenLostID, { loaders: { loadLiquidatee: true } });
 });
 
 CDAIContract_LiquidateBorrow_handler(({ event, context }) => {
+  let liquidatorAddress: string = event.params.liquidator.toString();
+  let liquidateeAddress: string = event.params.borrower.toString();
+  let ctokenAddress: string = event.params.cTokenCollateral.toString();
+  let tokenWonID: string = liquidatorAddress.concat(ctokenAddress);
+  let tokenLostID: string = liquidateeAddress.concat(ctokenAddress);
 
-  let liquidator = context.liquidator.get(event.params.liquidator.toString());
-
+  // update/create liquidator
+  let liquidator = context.liquidator.get(liquidatorAddress);
   if (!!liquidator) {
     const updatedLiquidator = {
       id: liquidator.id,
+      numberLiquidations: liquidator.numberLiquidations + 1
     }
     context.liquidator.set(updatedLiquidator);
   } else {
     const liquidatorObject = {
       id: event.params.liquidator.toString(),
+      numberLiquidations: 1
     }
     context.liquidator.set(liquidatorObject);
   }
-});
+
+  // update/create liquidatee
+  let liquidatee = context.liquidatee.get(liquidateeAddress);
+  if (!!liquidatee) {
+    const updatedLiquidatee = {
+      id: liquidatee.id,
+      numberLiquidations: liquidatee.numberLiquidations + 1
+    }
+    context.liquidatee.set(updatedLiquidatee);
+  } else {
+    const liquidateeObject = {
+      id: event.params.borrower.toString(),
+      numberLiquidations: 1
+    }
+    context.liquidatee.set(liquidateeObject);
+  }
+
+  // update/create TokenWon
+  let tokenWon = context.tokenWon.get(tokenWonID);
+  if (!!tokenWon) {
+    const updatedTokenWon = {
+      id: tokenWon.id,
+      ctoken: event.params.cTokenCollateral,
+      liquidator: tokenWon.liquidator,
+      amountWon: tokenWon.amountWon + event.params.seizeTokens
+    }
+    context.tokenWon.set(updatedTokenWon)
+  }
+  else { // TODO}
+    // is this slow?  Could I just set this to liquidatorAddress instead?
+  });
 
