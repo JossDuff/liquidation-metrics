d5611b6789bfea88901765634388520caee36e20
jossduff <jduff360@gmail.com>
2023-09-19 12:35:15 -0400

    we're cooking

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -28,87 +28,87 @@ CDAIContract_LiquidateBorrow_loader(({ event, context }) => {
   context.liquidatoraccount.load(liquidatorAddress);
   context.liquidatedaccount.load(liquidatedAddress);
   context.tokenwon.load(tokenwonID, {
-    loaders: { loadLiquidatorAccountX: true },
+    loaders: { loadLiquidatorAccountWon: true },
   });
   context.tokenlost.load(tokenlostID, {
-    loaders: { loadLiquidatedAccountX: true },
+    loaders: { loadLiquidatedAccountLost: true },
   });
 });
 
 CDAIContract_LiquidateBorrow_handler(({ event, context }) => {
-  // let liquidatorAddress: string = event.params.liquidator.toString();
-  // let liquidatedAddress: string = event.params.borrower.toString();
+  let liquidatorAddress: string = event.params.liquidator.toString();
+  let liquidatedAddress: string = event.params.borrower.toString();
   // context.log.debug(`processing liquidator ${liquidatorAddress}`);
-  // // update/create liquidator
-  // let liquidatorAccount = context.liquidatorAccount.get(liquidatorAddress);
-  // if (!!liquidatorAccount) {
-  //   const updatedLiquidatorAccount: liquidatoraccountEntity = {
-  //     id: liquidatorAccount.id,
-  //     numberLiquidations: liquidatorAccount.numberLiquidations + 1,
-  //   };
-  //   context.liquidatorAccount.set(updatedLiquidatorAccount);
-  // } else {
-  //   const liquidatorAccountObject: liquidatoraccountEntity = {
-  //     id: liquidatorAddress,
-  //     numberLiquidations: 1,
-  //   };
-  //   context.liquidatorAccount.set(liquidatorAccountObject);
-  // }
+  // update/create liquidator
+  let liquidatorAccount = context.liquidatoraccount.get(liquidatorAddress);
+  if (!!liquidatorAccount) {
+    const updatedLiquidatorAccount: liquidatoraccountEntity = {
+      id: liquidatorAccount.id,
+      numberLiquidations: liquidatorAccount.numberLiquidations + 1,
+    };
+    context.liquidatoraccount.set(updatedLiquidatorAccount);
+  } else {
+    const liquidatorAccountObject: liquidatoraccountEntity = {
+      id: liquidatorAddress,
+      numberLiquidations: 1,
+    };
+    context.liquidatoraccount.set(liquidatorAccountObject);
+  }
   // context.log.debug(`processing liquidated ${liquidatedAddress}`);
-  // // update/create liquidatedAccount
-  // let liquidatedAccount = context.liquidatedaccount.get(liquidatedAddress);
-  // if (!!liquidatedAccount) {
-  //   const updatedLiquidatedAccount: liquidatedaccountEntity = {
-  //     id: liquidatedAccount.id,
-  //     numberLiquidations: liquidatedAccount.numberLiquidations + 1,
-  //   };
-  //   context.liquidatedaccount.set(updatedLiquidatedAccount);
-  // } else {
-  //   const liquidatedAccountObject: liquidatedaccountEntity = {
-  //     id: liquidatedAddress,
-  //     numberLiquidations: 1,
-  //   };
-  //   context.liquidatedaccount.set(liquidatedAccountObject);
-  // }
-  // let ctokenAddress: string = event.params.cTokenCollateral.toString();
-  // let tokenwonID: string = liquidatorAddress.concat(ctokenAddress);
-  // let tokenlostID: string = liquidatedAddress.concat(ctokenAddress);
-  // // update/create tokenwon
-  // let tokenwon = context.tokenwon.get(tokenwonID);
-  // if (!!tokenwon) {
-  //   const updatedtokenwon: tokenwonEntity = {
-  //     id: tokenwon.id,
-  //     ctoken: tokenwon.ctoken,
-  //     liquidatorAccount: tokenwon.liquidatorAccount,
-  //     amountWon: tokenwon.amountWon + event.params.seizeTokens,
-  //   };
-  //   context.tokenwon.set(updatedtokenwon);
-  // } else {
-  //   const tokenwonObject: tokenwonEntity = {
-  //     id: tokenwonID,
-  //     ctoken: ctokenAddress,
-  //     liquidatorAccount: liquidatorAddress,
-  //     amountWon: event.params.seizeTokens,
-  //   };
-  //   context.tokenwon.set(tokenwonObject);
-  // }
-  // // update/create tokenlost
-  // let tokenlost = context.tokenlost.get(tokenlostID);
-  // if (!!tokenlost) {
-  //   const updatedtokenlost: tokenlostEntity = {
-  //     id: tokenlost.id,
-  //     ctoken: tokenlost.ctoken,
-  //     liquidatedAccount: tokenlost.liquidatedAccount,
-  //     amountLost: tokenlost.amountLost + event.params.seizeTokens,
-  //   };
-  //   context.tokenlost.set(updatedtokenlost);
-  // } else {
-  //   const tokenlostObject: tokenlostEntity = {
-  //     id: tokenlostID,
-  //     ctoken: ctokenAddress,
-  //     liquidatedAccount: liquidatedAddress,
-  //     amountLost: event.params.seizeTokens,
-  //   };
-  //   context.tokenlost.set(tokenlostObject);
-  // }
+  // update/create liquidatedAccount
+  let liquidatedAccount = context.liquidatedaccount.get(liquidatedAddress);
+  if (!!liquidatedAccount) {
+    const updatedLiquidatedAccount: liquidatedaccountEntity = {
+      id: liquidatedAccount.id,
+      numberLiquidations: liquidatedAccount.numberLiquidations + 1,
+    };
+    context.liquidatedaccount.set(updatedLiquidatedAccount);
+  } else {
+    const liquidatedAccountObject: liquidatedaccountEntity = {
+      id: liquidatedAddress,
+      numberLiquidations: 1,
+    };
+    context.liquidatedaccount.set(liquidatedAccountObject);
+  }
+  let ctokenAddress: string = event.params.cTokenCollateral.toString();
+  let tokenwonID: string = liquidatorAddress.concat(ctokenAddress);
+  let tokenlostID: string = liquidatedAddress.concat(ctokenAddress);
+  // update/create tokenwon
+  let tokenwon = context.tokenwon.get(tokenwonID);
+  if (!!tokenwon) {
+    const updatedtokenwon: tokenwonEntity = {
+      id: tokenwon.id,
+      ctoken: tokenwon.ctoken,
+      liquidatorAccountWon: tokenwon.liquidatorAccountWon,
+      amountWon: tokenwon.amountWon + event.params.seizeTokens,
+    };
+    context.tokenwon.set(updatedtokenwon);
+  } else {
+    const tokenwonObject: tokenwonEntity = {
+      id: tokenwonID,
+      ctoken: ctokenAddress,
+      liquidatorAccountWon: liquidatorAddress,
+      amountWon: event.params.seizeTokens,
+    };
+    context.tokenwon.set(tokenwonObject);
+  }
+  // update/create tokenlost
+  let tokenlost = context.tokenlost.get(tokenlostID);
+  if (!!tokenlost) {
+    const updatedtokenlost: tokenlostEntity = {
+      id: tokenlost.id,
+      ctoken: tokenlost.ctoken,
+      liquidatedAccountLost: tokenlost.liquidatedAccountLost,
+      amountLost: tokenlost.amountLost + event.params.seizeTokens,
+    };
+    context.tokenlost.set(updatedtokenlost);
+  } else {
+    const tokenlostObject: tokenlostEntity = {
+      id: tokenlostID,
+      ctoken: ctokenAddress,
+      liquidatedAccountLost: liquidatedAddress,
+      amountLost: event.params.seizeTokens,
+    };
+    context.tokenlost.set(tokenlostObject);
+  }
 });
